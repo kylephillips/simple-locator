9a8910a532a2d2afd34766d214be8a73a8a03d8b
Kyle Phillips <kylephillipsdesign@gmail.com>
2014-10-23 20:47:58 -0400

    Activation logic moved to separate class.

--- includes/class-simple-locator.php
+++ includes/class-simple-locator.php
@@ -1,4 +1,5 @@
 <?php
+require_once('class-sl-activation.php');
 require_once('class-sl-shortcode.php');
 require_once('class-sl-formhandler.php');
 require_once('class-sl-settings.php');
@@ -11,20 +12,11 @@ require_once('class-sl-dependencies.php');
 */
 class SL_SimpleLocator {
 
-	/**
-	* Plugin Version
-	*/
-	private $version;
-
-
 	function __construct()
 	{
-		$this->version = '1.0';
-		$this->set_version();
 		$this->init();
 		$this->form_action();
-		$this->set_default_options();
-		add_filter( 'plugin_action_links_' . 'wpsimplelocator/wpsimplelocator.php', array($this, 'settings_link' ) );
+		add_filter( 'plugin_action_links_' . 'wp-simple-locator/simplelocator.php', array($this, 'settings_link' ) );
 		add_action('init', array($this, 'add_localization') );
 	}
 
@@ -34,25 +26,12 @@ class SL_SimpleLocator {
 	*/
 	public function init()
 	{
-		$dependencies = new SL_Dependencies;
-		$post_type = new SL_PostType;
-		$meta_fields = new SL_MetaFields;
-		$settings = new SL_Settings;
-		$shortcode = new SL_Shortcode;
-	}
-
-
-	/**
-	* Check/update the version number in the DB
-	*/
-	public function set_version()
-	{
-		if ( !get_option('wpsl_version') ){
-			update_option('wpsl_version', $this->version);
-		}
-		elseif ( get_option('wpsl_version') < $this->version ){
-			update_option('wpsl_version', $this->version);	
-		}
+		new SL_Activation;
+		new SL_Dependencies;
+		new SL_PostType;
+		new SL_MetaFields;
+		new SL_Settings;
+		new SL_Shortcode;
 	}
 
 
@@ -64,26 +43,6 @@ class SL_SimpleLocator {
 		if ( is_admin() ) {
 			add_action( 'wp_ajax_nopriv_locate', 'wpsl_form_handler' );
 			add_action( 'wp_ajax_locate', 'wpsl_form_handler' );
-		}
-	}
-
-
-	/**
-	* Set Default Options
-	*/
-	public function set_default_options()
-	{
-		if ( !get_option('wpsl_post_type') ){
-			update_option('wpsl_post_type', 'location');
-		}
-		if ( !get_option('wpsl_field_type') ){
-			update_option('wpsl_field_type', 'wpsl');
-		}
-		if ( !get_option('wpsl_lat_field') ){
-			update_option('wpsl_lat_field', 'wpsl_latitude');
-		}
-		if ( !get_option('wpsl_lng_field') ){
-			update_option('wpsl_lng_field', 'wpsl_longitude');
 		}
 	}
 
---
+++ includes/class-sl-activation.php
@@ -0,0 +1,81 @@
+<?php
+
+class SL_Activation {
+
+	/**
+	* Plugin Version
+	*/
+	private $version;
+
+	public function __construct()
+	{
+		register_activation_hook( dirname( dirname(__FILE__) ) . '/simplelocator.php', array($this, 'install') );
+		$this->version = 1.0;
+		$this->setVersion();
+	}
+
+
+	/**
+	* Activation Hook
+	*/
+	public function install()
+	{
+		$this->checkVersions();
+		$this->setOptions();
+	}
+
+
+	/**
+	* Check Wordpress and PHP versions
+	*/
+	private function checkVersions( $wp = '3.8', $php = '5.3.0' ) {
+		global $wp_version;
+		if ( version_compare( PHP_VERSION, $php, '<' ) )
+			$flag = 'PHP';
+		elseif ( version_compare( $wp_version, $wp, '<' ) )
+			$flag = 'WordPress';
+		else 
+			return;
+		$version = 'PHP' == $flag ? $php : $wp;
+		deactivate_plugins( basename( __FILE__ ) );
+		
+		wp_die('<p><strong>Simple Locator</strong> plugin requires'.$flag.'  version '.$version.' or greater.</p>','Plugin Activation Error',  array( 'response'=>200, 'back_link'=>TRUE ) );
+	}
+
+
+
+	/**
+	* Set the Plugin Version
+	*/
+	private function setVersion()
+	{
+		if ( !get_option('wpsl_version') ){
+			update_option('wpsl_version', $this->version);
+		}
+		elseif ( get_option('wpsl_version') < $this->version ){
+			update_option('wpsl_version', $this->version);	
+		}
+	}
+
+
+	/**
+	* Set Default Options
+	*/
+	private function setOptions()
+	{
+		if ( !get_option('wpsl_post_type') ){
+			update_option('wpsl_post_type', 'location');
+		}
+		if ( !get_option('wpsl_field_type') ){
+			update_option('wpsl_field_type', 'wpsl');
+		}
+		if ( !get_option('wpsl_lat_field') ){
+			update_option('wpsl_lat_field', 'wpsl_latitude');
+		}
+		if ( !get_option('wpsl_lng_field') ){
+			update_option('wpsl_lng_field', 'wpsl_longitude');
+		}
+	}
+
+
+}

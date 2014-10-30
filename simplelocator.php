5d46d6fe5c7a7f1da0bf0784ae19072a60a5c3fa
Kyle Phillips <kylephillipsdesign@gmail.com>
2014-10-29 22:31:28 -0400

    Composer added, includes directory renamed in prep for autoloading.

--- simplelocator.php
+++ simplelocator.php
@@ -24,7 +24,8 @@ License: GPLv2 or later.
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
+require('vendor/autoload.php');
 if ( !class_exists('WPSL_SimpleLocator') ) :
-	require_once('includes/class-simple-locator.php');
+	require_once('SimpleLocator/class-simple-locator.php');
 	$wpsimplelocator = new WPSL_SimpleLocator;
 endif;

9a511964be0516bcbe46203ace7a933d6a64472a
Kyle Phillips <kylephillipsdesign@gmail.com>
2015-02-07 15:05:43 -0500

    JS added to load all locations map

--- assets/js/source/simple-locator-all-locations.js
+++ assets/js/source/simple-locator-all-locations.js
@@ -10,22 +10,54 @@ jQuery(function($){
 	function loadmap()
 	{
 		var locations = wpsl_locator_all.locations;
-		console.log(locations);
+		var mapstyles = wpsl_locator.mapstyles;	
+		var mappin = ( wpsl_locator.mappin ) ? wpsl_locator.mappin : '';
+		var bounds = new google.maps.LatLngBounds();
+		var mapOptions = {
+				mapTypeId: 'roadmap',
+				mapTypeControl: false,
+				zoom: 8,
+				styles: mapstyles,
+				panControl : false
+			}
+		var infoWindow = new google.maps.InfoWindow(), marker, i;
+		var map = new google.maps.Map( document.getElementById('alllocationsmap'), mapOptions );
+		
+		// Loop through array of markers & place each one on the map  
+		for( i = 0; i < locations.length; i++ ) {
+			var position = new google.maps.LatLng(locations[i].latitude, locations[i].longitude);
+			bounds.extend(position);
+			
+			var marker = new google.maps.Marker({
+				position: position,
+				map: map,
+				title: locations[i].title,
+				icon: mappin
+			});	
 
-		// var mappin = ( wpsl_locator.mappin ) ? wpsl_locator.mappin : '';
-		// var position = new google.maps.LatLng( parseFloat(wpsl_locator_single.latitude), parseFloat(wpsl_locator_single.longitude) );
-		// var options = {
-		// 	center: position,
-		// 	zoom: 12,
-		// 	styles: wpsl_locator.mapstyles
-		// };
-		// var map = new google.maps.Map( document.getElementById('locationmap') , options );
-		// var marker = new google.maps.Marker({
-		// 	position: position,
-		// 	map: map,
-		// 	icon: mappin,
-		// 	title: wpsl_locator_single.title
-		// });
-	}
+			// Info window for each marker 
+			google.maps.event.addListener(marker, 'click', (function(marker, i){
+				return function() {
+					infoWindow.setContent('<h4>' + locations[i].title + '</h4><p><a href="' + locations[i].permalink + '">' + wpsl_locator.viewlocation + '</a></p>');
+					infoWindow.open(map, marker);
+				}
+			})(marker, i));
+			
+			// Center the Map
+			map.fitBounds(bounds);
+			var listener = google.maps.event.addListener(map, "idle", function() { 
+					if ( data.results.length < 2 ) {
+					map.setZoom(13);
+				}
+				google.maps.event.removeListener(listener); 
+			});
+		}
+
+		// Fit the map bounds to all the pins
+		var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+			google.maps.event.removeListener(boundsListener);
+		});
+
+	} // loadmap()
 
 });

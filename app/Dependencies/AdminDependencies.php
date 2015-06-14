5030ee899ddf9250a6c5d4a1e6ec976d592438e3
Kyle Phillips <kylephillipsdesign@gmail.com>
2015-06-14 15:33:24 -0400

    Bug fix on location post entry screen, Map script not being enqueued on CPTs.

--- app/Dependencies/AdminDependencies.php
+++ app/Dependencies/AdminDependencies.php
@@ -36,7 +36,7 @@ class AdminDependencies extends DependencyBase
 	public function scripts()
 	{
 		$screen = get_current_screen();
-		if ( ($screen->post_type == $this->post_type) || ($screen->id == 'settings_page_wp_simple_locator') ) {
+		if ( ($screen->post_type == get_option('wpsl_post_type')) || ($screen->id == 'settings_page_wp_simple_locator') ) {
 			$this->addGoogleMaps();
 			wp_enqueue_script('google-maps');
 			wp_enqueue_script('media-upload');

a33e829957772945fd2e55681051ee70c7ffd2b8
Kyle Phillips <kylephillipsdesign@gmail.com>
2015-07-09 21:21:03 -0400

    Version number update

--- app/SimpleLocator.php
+++ app/SimpleLocator.php
@@ -12,7 +12,7 @@ class SimpleLocator
 		$ls_env = 'live';
 
 		global $simple_locator_version;
-		$simple_locator_version = '1.3.0';
+		$simple_locator_version = '1.3.1';
 
 		$app = new SimpleLocator\Bootstrap;
 	}

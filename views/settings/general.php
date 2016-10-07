273476a87e3b1ab203346f4e18892cee059e711e
Kyle Phillips <kyle@object9.com>
2016-10-07 11:05:38 -0400

    Settings page API key message updated.

--- views/settings/general.php
+++ views/settings/general.php
@@ -9,7 +9,7 @@ if ( !isset($location_btn['text']) || $location_btn['text'] == "" ) $location_bt
 
 <div class="wpsl-settings-general-head">
 	<h3><?php _e('Google Maps API Key', 'wpsimplelocator'); ?></h3>
-	<p><?php _e('While not required, it\'s a good idea to sign up for a Google Maps API key. If your website receives a spike in traffic, this will allow you to monitor the usage of the API and possibly prevent the cutoff of access.', 'wpsimplelocator'); ?> <a href="https://developers.google.com/maps/documentation/javascript/tutorial#api_key" target="_blank"><?php _e('How to obtain an API key', 'wpsimplelocator'); ?></a></p>
+	<p><?php _e('As of June 2016, all websites using the Google Maps API require a valid API key.', 'wpsimplelocator'); ?> <a href="https://developers.google.com/maps/documentation/javascript/tutorial#api_key" target="_blank"><?php _e('How to obtain an API key', 'wpsimplelocator'); ?></a></p>
 	<p>
 		<strong><label for="wpsl_google_api_key"><?php _e('Google Maps Javascript API V3 Key (public)', 'wpsimplelocator');?></label></strong><br>
 		<input type="text" name="wpsl_google_api_key" id="wpsl_google_api_key" value="<?php echo get_option('wpsl_google_api_key'); ?>" />

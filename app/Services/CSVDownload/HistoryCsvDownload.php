a9f98aaef7f015b13eb02ee243e5fff3e0b0be7b
Kyle Phillips <kylephillipsdesign@gmail.com>
2016-11-19 20:09:29 -0500

    CSV download/generation added.

--- app/Services/CSVDownload/HistoryCsvDownload.php
+++ app/Services/CSVDownload/HistoryCsvDownload.php
@@ -2,6 +2,7 @@
 namespace SimpleLocator\Services\CSVDownload;
 
 use SimpleLocator\Repositories\SearchHistoryRepository;
+use League\Csv\Writer;
 
 class HistoryCsvDownload
 {
@@ -19,12 +20,48 @@ class HistoryCsvDownload
 	{
 		$this->search_history_repo = new SearchHistoryRepository;
 		$this->getResults();
+		$this->generateCsv();
 	}
 
+	/**
+	* Get the Search History based on post params
+	*/
 	private function getResults()
 	{
-		var_dump($_POST);
 		$this->results = $this->search_history_repo->setSearchHistory('POST');
-		var_dump($this->results); die();
+	}
+
+	/**
+	* Generate the CSV
+	*/
+	private function generateCsv()
+	{
+		$csv = Writer::createFromFileObject(new \SplTempFileObject());
+		
+		// Header Row
+		$csv->insertOne([
+			__('Time', 'wpsimplelocator'), 
+			__('User IP', 'wpsimplelocator'), 
+			__('Search Latitude', 'wpsimplelocator'), 
+			__('Search Longitude', 'wpsimplelocator'), 
+			__('Search Term', 'wpsimplelocator'), 
+			__('Search Term Formatted', 'wpsimplelocator'), 
+			__('Distance', 'wpsimplelocator')
+		]);
+
+		foreach ( $this->results as $result ){
+			$csv->insertOne([
+				$result->time, 
+				$result->user_ip, 
+				$result->search_lat, 
+				$result->search_lng, 
+				$result->search_term, 
+				$result->search_term_formatted, 
+				$result->distance
+			]);
+		}
+
+		$filename = __('location-search-history', 'wpsimplelocator') . '.csv';
+		$csv->output($filename);
 	}
 }

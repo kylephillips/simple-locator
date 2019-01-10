b19e1b6117a7c3504c89fe998b6f3d87adad937b
Kyle Phillips <kyle@object9.com>
2019-01-10 17:29:59 -0500

    Updates license copyright year

--- simplelocator.php
+++ simplelocator.php
@@ -11,7 +11,7 @@ Domain Path: /languages/
 License: GPLv2 or later.
 */
 
-/*  Copyright 2018 Kyle Phillips
+/*  Copyright 2019 Kyle Phillips
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License, version 2, as 

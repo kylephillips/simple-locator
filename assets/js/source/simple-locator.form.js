cbc78bd5f4c4aac39341c1c8179d377cf5cf6551
Kyle Phillips <kylephillipsdesign@gmail.com>
2019-01-14 06:52:54 -0500

    Fixes issue where custom meta data was not being declared correctly in Javascript

--- assets/js/source/simple-locator.form.js
+++ assets/js/source/simple-locator.form.js
@@ -160,7 +160,7 @@ SimpleLocator.Form = function()
 		if ( wpsl_locator.postfields.length == 0 ) return
 		for ( var i = 0; i < wpsl_locator.postfields.length; i++ ){
 			var field = wpsl_locator.postfields[i];
-			formdata[field] = $('input[name=' + field + ']').val();
+			self.formData[field] = $('input[name=' + field + ']').val();
 		}
 	}
 

ff0ca7bf7f9b8be3509bccecd7332835dda1a0c8
Kyle Phillips <kyle@object9.com>
2021-05-03 11:43:59 -0400

    Removes testing address

--- app/Services/Import/GoogleMapGeocode.php
+++ app/Services/Import/GoogleMapGeocode.php
@@ -39,8 +39,6 @@ class GoogleMapGeocode
 		]);
 		$response_status = $response->getReasonPhrase();
 
-		//if ( $address == '5395 Sugarloaf Pkwy Lawrenceville GA 30043' ) $response_status = 'OVER_QUERY_LIMIT';
-
 		if ( $response_status == 'OVER_QUERY_LIMIT' ){
 			throw new GoogleQueryLimitException(__('Your API limit has been reached. Try again in 24 hours.', 'simple-locator'));
 		}
